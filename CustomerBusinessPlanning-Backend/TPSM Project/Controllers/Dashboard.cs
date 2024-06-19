bcd895473d38bfeabd67a4ca7592177a0f82b80c
SinhaSoumya19 <soumya.sinha@capgemini.com>
2024-06-19 15:55:14 +0530

    Add files via upload

---
+++ CustomerBusinessPlanning-Backend/TPSM Project/Controllers/Dashboard.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace TPSM_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Dashboard : ControllerBase
+    {
+        // List for RetailersName
+        List<string> retailersName = new List<string> { "Rohan", "Chandra", "Soumya", "Urmy", "Arjun" };
+
+        [HttpGet("RetailersName")]
+        public ActionResult RetailersName()
+        {
+            return Ok(retailersName);
+        }
+
+        // List for RetailersBanner
+        List<string> retailersBanner = new List<string> { "Banner1", "Banner2", "Banner3", "Banner4", "Banner5" };
+
+        [HttpGet("RetailersBanner")]
+        public ActionResult RetailersBanner()
+        {
+            return Ok(retailersBanner);
+        }
+
+        // List for CategoryName
+        List<string> categoryName = new List<string> { "Electronics", "Grocery", "Clothing", "Furniture", "Toys" };
+
+        [HttpGet("CategoryName")]
+        public ActionResult CategoryName()
+        {
+            return Ok(categoryName);
+        }
+
+        // List for PromotedGroupName
+        List<string> promotedGroupName = new List<string> { "Group1", "Group2", "Group3", "Group4", "Group5" };
+
+        [HttpGet("PromotedGroupName")]
+        public ActionResult PromotedGroupName()
+        {
+            return Ok(promotedGroupName);
+        }
+
+        // List for PlanYear
+        List<string> planYear = new List<string> { "2021", "2022", "2023", "2024", "2025" };
+
+        [HttpGet("PlanYear")]
+        public ActionResult PlanYear()
+        {
+            return Ok(planYear);
+        }
+    }
+}
---
+++ CustomerBusinessPlanning-Backend/TPSM Project/Program.cs
@@ -0,0 +1,45 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// Enable CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyAllowSpecificOrigins",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000")// Add your client app's URL here
+                                             //"http://www.example.com")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("MyAllowSpecificOrigins"); // Make sure this is before UseRouting and UseAuthorization
+app.UseRouting();
+
+app.UseAuthorization();
+
+
+
+app.MapControllers();
+
+app.Run();
